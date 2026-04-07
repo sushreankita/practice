3e5ce5093df960ac2be997a3f895443ea6594f09
sushreankita <sushreeankita47@gmail.com>
2026-04-07 23:10:09 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,46 @@
+#wap to check if num is even
+#num=int(input('enter a number: '))
+#if num%2==0:
+   # print(f"the number you choosed ie {num} it is a even number)
+
+
+   
+
+#wap to check if data is float
+#data=eval(input('enter a number: '))
+#if type (data) == float:
+ #   print ('data entered is float value')
+
+
+ 
+
+ #wap to check weather the given number is multiple of 3
+#num=int(input('enter a number: '))
+#if num%3 == 0:
+     #print("this number is multiple of 3")
+
+ 
+#wap to check weather the character is vowel
+##char=input('enter a char:')
+##vowel = 'aeiouAEIOU'
+##if char in vowel:
+##    print('this char is vowel')
+
+
+ 
+#wap to check if data is single value datatype
+##data=eval(input('enter a data :'))
+##if type(data)in [int,float,complex,bool]:
+## print(f'{data} is single valued datatype')
+
+ 
+#wap to get the ascii value of character if it is in uppercase
+##char=input('enter a char: ')
+##if char >= 'A' and char <= 'Z' :
+##    print(ord(char))
+
+
+ 
+
+
+ 
